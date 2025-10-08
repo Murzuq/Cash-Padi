6f1c86d309e4a2654dba3a30fff58ac905043f42
Murzuq Alabi <murzuqalabi@gmail.com>
2025-10-08 19:45:45 +0200

    Update API

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,14 @@ const app = express();
 // Middleware
 app.use(cors()); // Enable Cross-Origin Resource Sharing
 app.use(express.json()); // Body parser for JSON
+const PORT = process.env.PORT || 5000;
 
 // Connect to Database
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected...");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
     console.error(err.message);
     process.exit(1);
@@ -33,7 +35,3 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/users", userRoutes); // 2. Tell Express to use the user routes
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
