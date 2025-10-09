9570c3f33d5fde636e19cefd0acc447d53688529
Murzuq Alabi <murzuqalabi@gmail.com>
2025-10-09 13:14:39 +0200

    Updated API

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+  "https://cash-padi.onrender.com" || "http://localhost:5000";
 
 export const API_ENDPOINTS = {
   GET_ME: `${API_URL}/api/users/me`,
