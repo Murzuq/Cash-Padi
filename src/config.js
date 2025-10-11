d038570009b4a9b2c82caf5427954d90961e3273
Murzuq Alabi <murzuqalabi@gmail.com>
2025-10-11 09:04:09 +0200

    Edited config.js file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:5000";
+export const API_URL =
+  "http://localhost:5000" || "https://cash-padi.onrender.com";
 // "https://cash-padi.onrender.com" || "http://localhost:5000";
 
 export const API_ENDPOINTS = {
